90ed5ae81b7b496f2d3d667f4c8e910d0aa847bf
nmuldavin <nmuldavin@gmail.com>
2016-05-09 14:44:20 -0700

    Finished Strings and Regex

--- app/regex.js
+++ app/regex.js
@@ -2,25 +2,32 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
   containsNumber : function(str) {
-
+    var regExp = /\d/;
+    return regExp.test(str);
   },
 
   containsRepeatingLetter : function(str) {
-
+    var regExp = /([a-zA-Z])\1/
+    return regExp.test(str);
   },
 
   endsWithVowel : function(str) {
-
+    var regExp = /[aeiou]$/i
+    return regExp.test(str);
   },
 
   captureThreeNumbers : function(str) {
-
+    var regExp = /\d{3}/
+    var result = regExp.exec(str);
+    return result ? result[0] : false
   },
 
   matchesPattern : function(str) {
-
+    var regExp = /^\d{3}-\d{3}-\d{4}$/
+    return regExp.test(str);
   },
   isUSD : function(str) {
-
+    var regExp = /^\$\d{1,3}(,\d{3})*(\.\d{2})?$/
+    return regExp.test(str);
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -2,12 +2,44 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    var out = '';
+    var previousChar;
+    var matches = 1;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === previousChar) {
+            matches += 1
+        } else {
+            matches = 1
+        }
+        if (matches <= amount) {
+            out += str[i];
+        }
+        previousChar = str[i];
+    }
+    return out;
   },
   wordWrap: function(str, cols) {
-
+    var words = str.split(" ");
+    var out = "";
+    var linelength = 0;
+    for (var i = 0; i < words.length; i++) {
+        if (linelength + words[i].length<= cols) {
+            if (linelength !== 0) {
+                out += " ";
+            }
+            out += words[i];
+            linelength += words[i].length;
+        } else {
+            if (linelength !== 0) {
+                out += "\n";
+            }
+            out += words[i];
+            linelength = words[i].length;
+        }
+    }
+    return out;
   },
   reverseString: function(str) {
-
+    return str.split('').reverse().join('');
   }
 };

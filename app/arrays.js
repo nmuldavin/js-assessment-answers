c5590f2e2f69b80bc04616fffffcd141bf241623
Noah Muldavin <nmuldavin@gmail.com>
2016-12-01 10:09:00 -0800

    Arrays with es6

--- app/arrays.js
+++ app/arrays.js
@@ -2,116 +2,126 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.arraysAnswers = {
 
-  indexOf : function(arr, item) {
-    var i;
-    for (i = 0; i < arr.length; i += 1) {
-      if (arr[i] === item) {
-        return i;
-      }
-    }
-
-    return -1;
+  indexOf(arr, item) {
+    return arr.indexOf(item);
   },
 
-  sum : function(arr) {
-    var i, sum = 0;
-    for(i = 0; i < arr.length; i += 1) {
-      sum += arr[i];
-    }
-
-    return sum;
+  sum(arr) {
+    return arr.reduce((sum, curr) => sum + curr, 0);
   },
 
-  remove : function(arr, item) {
-    var i, out=[];
-    for(i = 0; i < arr.length; i += 1) {
-      if (arr[i] !== item) {
-        out.push(arr[i]);
-      }
-    }
-
-    return out;
+  remove(arr, item) {
+    return arr.filter(entry => entry !== item);
   },
 
-  removeWithoutCopy : function(arr, item) {
-    var i;
-    for(i = arr.length - 1; i >= 0; i -= 1) {
+  removeWithoutCopy(arr, item) {
+    let i = 0;
+    while (arr[i]) {
       if (arr[i] === item) {
         arr.splice(i, 1);
+      } else {
+        ++i;
       }
     }
-
     return arr;
   },
 
-  append : function(arr, item) {
+  append(arr, item) {
     arr.push(item);
     return arr;
   },
 
-  truncate : function(arr) {
+  truncate(arr) {
     arr.pop();
     return arr;
   },
 
-  prepend : function(arr, item) {
+  prepend(arr, item) {
     arr.unshift(item);
     return arr;
   },
 
-  curtail : function(arr) {
+  curtail(arr) {
     arr.shift();
     return arr;
   },
 
-  concat : function(arr1, arr2) {
+  concat(arr1, arr2) {
     return arr1.concat(arr2);
   },
 
-  insert : function(arr, item, index) {
+  insert(arr, item, index) {
     arr.splice(index, 0, item);
     return arr;
   },
 
-  count : function(arr, item) {
-    var i, count = 0;
-    for(i = 0; i < arr.length; i++) {
-      if (arr[i] === item) {
-        count += 1;
+  count(arr, item) {
+    return arr.reduce((count, curr) => {
+      if (curr === item) {
+        return count + 1;
       }
-    }
-
-    return count;
+      return count;
+    }, 0);
   },
 
-  duplicates : function(arr) {
-    var i, uniquelist = [], duplicates = [];
-    for(i = 0; i < arr.length; i++) {
+  /**
+   * What is done here depends on how general elements of array are. The test only uses
+   * numbers so this is safe, otherwise you would need some kind of hashing
+   */
+  duplicates(arr) {
+    // map kept for quick access
+    let dups = {};
+    // array will be returned at end
+    let dupsArr = [];
+    let uniques = {};
 
-      if (uniquelist.indexOf(arr[i]) === -1) {
-        uniquelist.push(arr[i]);
-      } else if (duplicates.indexOf(arr[i]) === -1) {
-        duplicates.push(arr[i]);
+    /**
+     * For each item - add to uniques map if not there already
+     * if not already in duplicates map, push to duplicates array
+     */
+    arr.forEach(item => {
+      if (!uniques[item]) {
+        uniques[item] = true;
+      } else if (!dups[item]) {
+        dups[item] = true;
+        dupsArr.push(item);
       }
-    }
+    });
 
-    return duplicates;
+    return dupsArr;
   },
 
-  square : function(arr) {
-    var sqr = function(value) {
-      return value*value;
-    }
-    return arr.map(sqr);
+  square(arr) {
+    return arr.map(x => x * x);
   },
 
-  findAllOccurrences : function(arr, target) {
-    var i, out = [];
-    for (i = 0; i < arr.length; i++) {
-      if (arr[i] === target) {
-        out.push(i);
+  findAllOccurrences(arr, target) {
+    /**
+     * Recursive Inner function to accumulate list of indices
+     * Repeatedly applies array.indexOf(value, startIndex)
+     * @param  {Array}  array       Array to search
+     * @param  {*}      value       Value to search for
+     * @param  {Number} startIndex  Index to start searching at
+     * @param  {Array}  entries     Accumulated array of indices
+     * @return {Array}              Accumuldated array of indices
+     */
+    function findIndexRecursive(array, value, startIndex, entries) {
+      // find index
+      let index = array.indexOf(value, startIndex);
+      // if found
+      if (index !== -1) {
+        // accumulate
+        entries.push(index);
+        // recurse
+        return findIndexRecursive(array, value, index + 1, entries);
       }
+      // if not found, return accumulation
+      return entries;
     }
-    return out;
-  }
+
+    /**
+     * Applying inner function starting at 0
+     */
+    return findIndexRecursive(arr, target, 0, []);
+  },
 };
